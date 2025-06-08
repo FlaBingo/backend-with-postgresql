e7bacd5ed2107374a7d94a751bffc567e0dbf82d
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-08 12:33:02 +0530

    feat: implement user and todo management with database setup and routing

--- SQLite/src/server.js
+++ SQLite/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js"
+import todoRoutes from "./routes/todoRoutes.js"
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -14,11 +16,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // console.log(__filename); // E:\vsCode\Postgresql\SQLite\src\server.js
 // console.log(__dirname);  //E:\vsCode\Postgresql\SQLite\src
-app.use(express.static(path.join(__dirname, '../public')))
+// but we want E:\vsCode\Postgresql\SQLite\public
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
 
 // Serving up the HTML file from the /public directory
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
+app.use("/auth", authRoutes);
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
